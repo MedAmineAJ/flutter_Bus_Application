7798848437047f668c9886ecfbad8bf97efa2cb3
MED amine <medamine.ajroud@gmail.com>
2025-11-11 00:20:08 +0100

    Ajout upload image Cloudinary avec nom/prénom dans l’authentification

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,16 @@ import 'package:essai_firebase/library_screen.dart';
 import 'package:essai_firebase/games_screen.dart';
 import 'package:essai_firebase/feedback_screen.dart';
 import 'package:essai_firebase/meteo_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: 'https://rqqaknntfjrbwoxpwsox.supabase.co',     // 🔸 à remplacer
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcWFrbm50ZmpyYndveHB3c294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTkwMTcsImV4cCI6MjA3ODM3NTAxN30.6wSaaz9lE5eVe8GU1s_wf6LQe0PGjfG1ep39z-OrYFQ',                 // 🔸 à remplacer
+  );
   runApp(const MyApp());
 }
 
